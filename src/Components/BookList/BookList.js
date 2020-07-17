35727d97cb5223c12db84cf83ee11fc3ff6e7a50
AxSch <alex_schuneman@outlook.com>
2020-07-17 17:51:51 +0100

    Refactor component & add styling
    Added renderBookGrid() to handle formatting on larger screens

--- src/Components/BookList/BookList.js
+++ src/Components/BookList/BookList.js
@@ -2,6 +2,8 @@ import React from 'react'
 import BookListItem from '../BookListItem/BookListItem'
 
 const BookList = ({ booksData }) => {
+    const bookArr1 = []
+    const bookArr2 = []
     const renderBookList = booksData => {
         return booksData.map((book, key) => {
             return (
@@ -9,11 +11,39 @@ const BookList = ({ booksData }) => {
             )
         })
     }
+    const renderBookGrid = booksData => {
+        if (booksData.length > 10) {
+           booksData.forEach(book => {
+               if (bookArr1.length !== 10) {
+                    bookArr1.push(book)
+               } else {
+                    bookArr2.push(book)
+               }
+           }); 
+        } else {
+            bookArr1.push(...booksData)
+        }
+    }
+    renderBookGrid(booksData)
     return (
-        <div>
-            <div>Showing {booksData.length} results</div>
-            <div>
-                {renderBookList(booksData)}
+        <div className="flex flex-row justify-end xl:w-screen flex-wrap">
+            <div className="flex flex-row mr-10 md:mr-8">Showing {booksData.length} results</div>
+            <div className="pb-6 lg:flex lg:flex-row lg:flex-wrap">
+                {booksData.length <= 10 ?
+                    <div className="hidden lg:flex lg:flex-col xl:w-screen">
+                        {renderBookList(bookArr1)}
+                    </div>
+                    : 
+                    <div className="hidden pl-4 lg:flex lg:flex-col lg:flex-1 xl:w-screen">
+                        {renderBookList(bookArr1)}
+                    </div>
+                }
+                <div className="hidden pr-4 lg:flex lg:flex-col lg:flex-1 xl:w-screen">
+                    {renderBookList(bookArr2)}
+                </div>
+                <div className="px-4 lg:hidden">
+                    {renderBookList(booksData)}
+                </div>
             </div>
         </div>
     )
