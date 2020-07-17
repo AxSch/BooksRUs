9e2ead9acaf024810dd0e76921e4845c1848ed42
AxSch <alex_schuneman@outlook.com>
2020-07-17 17:50:48 +0100

    Add & compile new classes

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundColor: theme => ({
+                'primary': '#1D7874',
+                'secondary': 'rgba(29, 120, 116, 0.16)',
+                'accent': '#29AAA4'
+            }),
+            fontSize: {
+                '7xl': '5rem',
+                '8xl': '6rem',
+                '9xl': '7rem',
+                'landing-md': '10rem',
+                'landing-lg': '15.875rem',
+            },
+            fontFamily: {
+                'display': ['Patua One'],
+                'sans-serif': ['Open Sans'],
+            },
+        }
+    },
+}
