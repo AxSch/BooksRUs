8aad66bce0e473090c36dd458692dc59bcc07d9c
AxSch <alex_schuneman@outlook.com>
2020-07-17 12:28:12 +0100

    Add BrowserRouter to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Book from './Containers/Books/Book';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Route exact path="/">
+          <Redirect to="books" />
+        </Route>
+        <Route path="/books" component={Book} />
+      </BrowserRouter>
   );
 }
 
