3d0c88cadc47d43a812e1fd65b9ec20fec5c656a
AxSch <alex_schuneman@outlook.com>
2020-07-18 17:11:53 +0100

    Refactor component
    Added id's for testing. Altered styling of component. Added
    conditional render for 0 case

--- src/Components/BookList/BookList.js
+++ src/Components/BookList/BookList.js
@@ -5,11 +5,19 @@ const BookList = ({ booksData }) => {
     const bookArr1 = []
     const bookArr2 = []
     const renderBookList = booksData => {
-        return booksData.map((book, key) => {
+        if (booksData.length > 0) {
+            return booksData.map((book, key) => {
+                return (
+                    <BookListItem book={book} key={key} />
+                )
+            })
+        } else {
             return (
-                <BookListItem book={book} key={key} />
+                <div className="flex flex-row justify-end lg:flex lg:flex-col lg:items-center text-gray-700 text-xl">
+                    No more results to show
+                </div>
             )
-        })
+        }
     }
     const renderBookGrid = booksData => {
         if (booksData.length > 10) {
@@ -26,22 +34,22 @@ const BookList = ({ booksData }) => {
     }
     renderBookGrid(booksData)
     return (
-        <div className="flex flex-row justify-end xl:w-screen flex-wrap">
-            <div className="flex flex-row mr-10 md:mr-8">Showing {booksData.length} results</div>
+        <div className="flex flex-col justify-end xl:w-screen flex-wrap items-center">
+            <div id="results" className="flex flex-row mr-10 md:mr-8 justify-end w-full pr-8">Showing {booksData.length} results</div>
             <div className="pb-6 lg:flex lg:flex-row lg:flex-wrap">
                 {booksData.length <= 10 ?
-                    <div className="hidden lg:flex lg:flex-col xl:w-screen">
+                    <div id="list10" className="hidden lg:flex lg:flex-col xl:w-screen">
                         {renderBookList(bookArr1)}
                     </div>
                     : 
-                    <div className="hidden pl-4 lg:flex lg:flex-col lg:flex-1 xl:w-screen">
+                    <div id="list10-1" className="hidden pl-4 lg:flex lg:flex-col lg:flex-1 xl:w-screen">
                         {renderBookList(bookArr1)}
                     </div>
                 }
-                <div className="hidden pr-4 lg:flex lg:flex-col lg:flex-1 xl:w-screen">
-                    {renderBookList(bookArr2)}
+                <div id="list10-2" className="hidden pr-4 mr-8 lg:flex lg:flex-col lg:flex-1 xl:w-screen">
+                    {booksData.length > 0 ? renderBookList(bookArr2) : ''}
                 </div>
-                <div className="px-4 lg:hidden">
+                <div className="pr-4 mr-4 lg:hidden">
                     {renderBookList(booksData)}
                 </div>
             </div>
